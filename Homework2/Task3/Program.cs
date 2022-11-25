c771ac06e477f9ba2599d25f282da29c23a0050f
Ivan Minakov <i.minakov786@gmail.com>
2022-11-25 06:09:45 -0500

    task 3 compleated

---
+++ Homework2/Task3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число дня недели");
+int a = Convert.ToInt32(Console.ReadLine());
+int saturday = 6;
+int sunday = 7;
+if (a == saturday || a == sunday) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
