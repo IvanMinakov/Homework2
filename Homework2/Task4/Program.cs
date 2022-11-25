5f6ea60ce1190a5be0afe619b4a70a58e394b491
Ivan Minakov <i.minakov786@gmail.com>
2022-11-25 13:54:17 -0500

    fizzbuzz DONE

---
+++ Homework2/Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = new int[100];
+int n = array.Length;
+for (int i = 0; i < n; i++)
+{
+    array[i] = i + 1;
+    if (array[i] % 3 == 0 & array[i] % 15 != 0)
+    {
+        Console.Write("Fizz, ");
+    }
+    else if (array[i] % 5 == 0 & array[i] % 15 != 0)
+    {
+        Console.Write("Buzz, ");
+    }
+    else if (array[i] % 15 == 0) 
+    {
+        Console.Write("FizzBuzz, ");
+    }
+    else
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
