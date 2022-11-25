0d70ce3b84a8c47248d1753c5bbb9ba154cf6d28
Ivan Minakov <i.minakov786@gmail.com>
2022-11-25 05:38:40 -0500

    task2 compleated

---
+++ Homework2/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int d = 0, k = 0;
+if (a > 99)
+{
+    k = a;
+    while (k >= 1000)
+    {
+        k /= 10;
+        d = k % 10;
+        Console.WriteLine($"Третье число слева {d} ");
+    }
+}
+else
+{
+    Console.WriteLine($"Третье числа нет");
+}
